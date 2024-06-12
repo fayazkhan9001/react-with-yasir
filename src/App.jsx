0a08d5d355ffd6591f59c1c17b2d5b5bfe7b61ec
fayazkhan <fayazkhan35119@gmail.com>
2024-06-12 12:48:37 +0500

    context

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { createContext } from "react";
 import Home from "./pages/Home";
 
+//What is useContext HOOK
+//create, Provider, useContext
+//How to use it.
+
+//1-create context
+const data = createContext();
+const data1 = createContext();
+
 const App = () => {
+  const name = "ali";
+  const age = 23;
   return (
     <div>
-      <Home />
+      {/* 2-Contex Provider */}
+      <data.Provider value={name}>
+        <data1.Provider value={age}>
+          <Home />
+        </data1.Provider>
+      </data.Provider>
     </div>
   );
 };
 
 export default App;
+
+//3-export
+export { data, data1 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,10 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
+import Child1 from "./Child1";
 
 const Home = () => {
-  const refElement = useRef();
-
-  const handleRef = () => {
-    refElement.current.focus();
-    refElement.current.value = "Gul khan";
-    refElement.current.style.color = "red";
-  };
   return (
     <div>
-      <h1>useRef() HOOK</h1>
-      <div>
-        <input ref={refElement} type="text" />
-        <button onClick={handleRef}>focusInput</button>
-      </div>
+      <Child1 />
     </div>
   );
 };
